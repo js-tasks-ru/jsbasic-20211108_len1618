913d98861b80ca28d6b6775c4addecedb73808b4
Notkabene <len1618@yandex.ru>
2021-11-21 23:37:04 +0300

    готово

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,7 @@
 function camelize(str) {
   // ваш код...
+  let letters = str.split('-').join('')
+  letters = str.toLowerCase().replace(/[^a-zA-Z0-9]+(.)/g, (m, chr) => chr.toUpperCase());
+  return letters
 }
+camelize('background-color')
