8b446cfdd42fe6dfa5cb6281b80cb1e6d558d933
Notkabene <len1618@yandex.ru>
2021-11-22 18:39:53 +0300

    готово

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,7 @@
 function getMinMax(str) {
   // ваш код...
+  let string =  str.split(' ').map(item => parseFloat(item)).filter(Boolean)
+  let min = string.sort((a, b) => a - b).shift();
+  let max = string.sort((a, b) => a - b).pop();
+  return result = {min, max}
 }
