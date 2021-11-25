356b69de2bcfa4550549ecf677b657016113061d
Notkabene <len1618@yandex.ru>
2021-11-25 23:25:07 +0300

    Исправила

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -2,6 +2,6 @@ function getMinMax(str) {
   // ваш код...
   let string =  str.split(' ').map(item => parseFloat(item)).filter(Boolean)
   let min = string.sort((a, b) => a - b).shift();
-  let max = string.sort((a, b) => a - b).pop();
+  let max = string.pop();
   return result = {min, max}
 }
