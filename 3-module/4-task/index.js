dd1eae022fd72f2073d81db659466475097257b2
Notkabene <len1618@yandex.ru>
2021-11-26 00:36:34 +0300

    готово

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,14 @@
 function showSalary(users, age) {
   // ваш код...
+  let usersString = '';
+  let newUsers = users.filter(user => user.age <= age);
+  let last = newUsers.length - 1;
+  newUsers.forEach((newUser, item) =>{
+    if(item == last){
+      usersString += `${newUser.name}, ${newUser.balance}`;
+    } else {
+      usersString += `${newUser.name}, ${newUser.balance}\n`;
+    }
+  });
+  return usersString;
 }
