78341273a3a9e937197ce5774b3ea52a530b5926
Notkabene <len1618@yandex.ru>
2021-12-07 23:22:32 +0300

    1 модуль готов

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -12,7 +12,47 @@
  *      }
  *
  */
+// console.clear();
+
 export default class UserTable {
+
   constructor(rows) {
+    
+    this.elem = document.createElement('table');
+
+    this.elem.innerHTML = `
+    <thead>
+    <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+    </tr>
+    </thead>
+    <tbody>
+
+        ${rows.map(row => `
+        <tr>
+          <td>${row.name}</td>
+          <td>${row.age}</td>
+          <td>${row.salary}</td>
+          <td>${row.city}</td>
+          <td><button class="delete">X</button></td>
+        </tr>`).join('')}
+    </tbody>`;
+
+    this.elem.addEventListener('click', (e) => {
+      const deleteButton = e.target.closest('.delete');
+      const parentTr = deleteButton.closest('tr');
+      
+      if (!deleteButton) {
+        return;
+      }
+      
+      parentTr.remove();
+    });
+  
   }
+  
 }
